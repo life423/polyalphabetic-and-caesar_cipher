e455c774c999173480009fa870116e3a622f25a3
Andrew Clark <drew@drewclark.io>
2023-09-12 06:12:43 -0500

    Refactor cipher_gui.py for improved user experience
    - Added a title label with a larger font size and bold style
    - Reorganized the layout of labels and entry fields for encryption/decryption options, cipher type, keyword/shift amount, and text input
    - Removed unnecessary print statement in clicked() function

--- cipher_gui.py
+++ cipher_gui.py
@@ -1,17 +1,34 @@
 import tkinter as tk
-from cipher_tool import caesar_cipher, poly_alphabetic_cipher, process_file
+from tkinter import StringVar
 
 root = tk.Tk()
 
-# Create an informative label
-info_label = tk.Label(root, text="Welcome to the Caesar and Polyalphabetic Cipher Tool!")
-info_label.pack()  # Place the label onto the window
+# Title
+title = tk.Label(root, text="Welcome to the Caesar and Polyalphabetic Cipher Tool!", font=('Helvetica', 16, 'bold'))
+title.pack(pady=10)
 
-def clicked():
-    # What happens when the button is clicked
-    print('Button clicked!')
+# Encryption/Decryption option
+action_label = tk.Label(root, text="Choose 'e' for encryption or 'd' for decryption: ")
+action_label.pack()
+action_entry = tk.Entry(root)
+action_entry.pack()
 
-click_me_button = tk.Button(root, text="Click me!", command=clicked)
-click_me_button.pack()  # Place the button onto the window
+# Cipher type option
+cipher_label = tk.Label(root, text="Enter 'c' for Caesar cipher or 'p' for Polyalphabetic cipher: ")
+cipher_label.pack()
+cipher_entry = tk.Entry(root)
+cipher_entry.pack()
+
+# Keyword/Shift amount
+keyword_label = tk.Label(root, text="Enter the keyword (for Polyalphabetic) or shift value (for Caesar): ")
+keyword_label.pack()
+keyword_entry = tk.Entry(root)
+keyword_entry.pack()
+
+# Text to be encrypted/decrypted
+text_label = tk.Label(root, text="Enter the text to be encrypted/decrypted: ")
+text_label.pack()
+text_entry = tk.Entry(root)
+text_entry.pack()
 
 root.mainloop()
