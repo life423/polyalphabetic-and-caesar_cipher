00cafc8843db4264a21d1ebb51d78c946b4916dc
Andrew Clark <drew@drewclark.io>
2023-09-12 06:20:22 -0500

    Refactor cipher_gui.py for improved functionality and user experience
    This commit adds new features to the GUI, including a result label and text box, as well as a button to process the entered text. The process_text() function has been implemented to handle input validation and execute the selected cipher operation. The result is displayed in the result_text box.
    
    The changes aim to enhance the usability of the application by providing real-time feedback on the processed text.

--- cipher_gui.py
+++ cipher_gui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import StringVar
+from cipher_tool import caesar_cipher, poly_alphabetic_cipher, process_file 
 
 root = tk.Tk()
 
@@ -30,5 +31,48 @@ text_label = tk.Label(root, text="Enter the text to be encrypted/decrypted: ")
 text_label.pack()
 text_entry = tk.Entry(root)
 text_entry.pack()
+
+result_label = tk.Label(root, text="Result: ")
+result_label.pack()
+
+result_text = tk.Text(root)
+result_text.pack()
+
+def process_text():
+    result_text.delete('1.0', tk.END)
+    # Retrieve input values
+    action = action_entry.get()
+    cipher_type = cipher_entry.get()
+    keyword_shift = keyword_entry.get()
+    text = text_entry.get()
+
+    # Validate inputs
+    if not (action in {'e', 'd'} and cipher_type in {'c', 'p'} and keyword_shift and text):
+        print("Invalid input. Please check all fields and try again.")
+        return
+
+    # Execute the selected cipher and operation
+    encrypted_decrypted_text = ''
+    if cipher_type == 'c':
+        shift = int(keyword_shift)
+        # Check whether to perform encryption or decryption
+        if action == 'e':
+            encrypted_decrypted_text = caesar_cipher(text, shift)
+        elif action == 'd':
+            encrypted_decrypted_text = caesar_cipher(text, shift, False)
+    elif cipher_type == 'p':
+        keyword = keyword_shift
+        # Check whether to perform encryption or decryption
+        if action == 'e':
+            encrypted_decrypted_text = poly_alphabetic_cipher(text, keyword)
+        elif action == 'd':
+            encrypted_decrypted_text = poly_alphabetic_cipher(text, keyword, False)
+
+    # Print the result
+    result_text.insert(tk.END, encrypted_decrypted_text)
+
+# Button to start processing
+process_button = tk.Button(root, text="Process Text", command=process_text)
+process_button.pack(pady=10)
 
 root.mainloop()
